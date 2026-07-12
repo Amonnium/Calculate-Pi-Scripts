5136187e55e674a188dc62a8459930c8c916bafd
João Diogo <158088433+Amonnium@users.noreply.github.com>
2026-07-12 11:20:41 +0100

    Remove .vscode directory in cleanup process

--- install.sh
+++ install.sh
@@ -54,6 +54,7 @@ rm Pi.bat
 rm -r Pi_in_PowerShell
 rm -r Assets/
 rm Dependencies/README.md/
+rm -r .vscode/
 rm Pi_in_C++_Folder/Pi_in_C++.exe/
 rm Pi_in_C_Folder/Pi_in_C.exe/
 rm Pi_in_C_Folder/Pi_in_C_2.exe/
